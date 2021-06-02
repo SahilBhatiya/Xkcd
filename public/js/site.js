dfb6b8ddc9f28f106c68a0dce6fe205c3a18b816
Sahil Bhatiya <sahil242bhatiya@gmail.com>
2021-06-02 17:53:42 +0530

    Improve Small Screen

--- public/js/site.js
+++ public/js/site.js
@@ -1,12 +1,24 @@
 window.onscroll = () => {
-    if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
+    if (screen.width > 425) {
+        if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
+            $(".Custom-Nav").css("bottom", "0px");
+            $(".Custom-Nav").css("opacity", "1");
+        } else {
+            $(".Custom-Nav").css("bottom", "-100px");
+            $(".Custom-Nav").css("opacity", "0");
+        }
+    } else {
         $(".Custom-Nav").css("bottom", "0px");
         $(".Custom-Nav").css("opacity", "1");
-    } else {
-        $(".Custom-Nav").css("bottom", "-100px");
-        $(".Custom-Nav").css("opacity", "0");
     }
 };
+
+window.onload = () => {
+    if (screen.width <= 425) {
+        $(".Custom-Nav").css("bottom", "0px");
+        $(".Custom-Nav").css("opacity", "1");
+    }
+}
 
 function RandomBtnClick() {
     $.post("/GenerateRandom", (data) => {
